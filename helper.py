1479cc5eb55285cdc00cbeb71e8f80aa30fe37e6
venkatrbalaji <venkatrbalaji@gmail.com>
2022-04-19 22:01:29 -0700

    Updated Scripts with 35 clusters and kmeans.pkl

--- helper.py
+++ helper.py
@@ -169,6 +169,7 @@ def get_most_rated_movies(user_movie_ratings, max_number_of_movies):
     user_movie_ratings = user_movie_ratings.append(user_movie_ratings.count(), ignore_index=True)
     # 2- sort
     user_movie_ratings_sorted = user_movie_ratings.sort_values(len(user_movie_ratings)-1, axis=1, ascending=False)
+    print(f"get_most_rated_movies() - Dropping: {user_movie_ratings_sorted.tail(1).index}")
     user_movie_ratings_sorted = user_movie_ratings_sorted.drop(user_movie_ratings_sorted.tail(1).index)
     # 3- slice
     # most_rated_movies = user_movie_ratings_sorted.iloc[:, :max_number_of_movies]
